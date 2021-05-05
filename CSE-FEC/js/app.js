b284e42665034571ecd9978a1ff123f8d28dda57
SouravBiswas238 <74732949+SouravBiswas238@users.noreply.github.com>
2021-05-05 16:01:07 +0600

    update=2

---
+++ CSE-FEC/js/app.js
@@ -0,0 +1,8 @@
+// const logoNav = document.querySelector(".logo-nav");
+// const navigation = document.querySelector(".navigation");
+
+// logoNav.addEventListener("click",() => {
+//     navigation.classList.toggle("open");
+// });
+
+
